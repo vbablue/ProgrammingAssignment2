311de16eac21df371453d1749b286bef2e71cd0b
vbablue <sriram.vadlamani@gmail.com>
2014-05-25 13:01:36 +0530

    Changes to first Draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 #This function caches the matrix that is passed. Calling this function should pass matrix as an argument. 
+#Returns a matrix
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -8,17 +9,17 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setInverseVector <- function(solve) m <<- solve
   getInverseVector <- function() m
-  matrix(set = set, get = get,
+  list(set = set, get = get,
        setInverseVector = setInverseVector,
-       getinversevector = getinversevector)
-  
-  
+       getInverseVector = getInverseVector)   
 }
 
+#This function caches the value of the inverse matrix when called multiple times to facilitate easy and fast retrieval from the memory. 
+#Returns a matrix
 cacheSolve <- function(x, ...) {
   m <- x$getInverseVector()
   if(!is.null(m)) {
-    message("getting cached data")
+    message("Getting cached data")
     return(m)
   }
   data <- x$get()
@@ -28,3 +29,14 @@ cacheSolve <- function(x, ...) {
   
 }
 
+#Test Cases
+amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+amatrix$get()         # Returns original matrix
+cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
+amatrix$getInverseVector()  # Returns matrix inverse
+cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
+
+amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
+amatrix$get()         # Returns matrix
+amatrix$getInverseVector()  # Returns matrix inverse
